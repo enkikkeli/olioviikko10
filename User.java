20ff0d0ef080aea06b4250bb23b8109885bb9230
enkikkeli <128397134+enkikkeli@users.noreply.github.com>
2023-04-03 20:18:34 +0300

    Create User.java

---
+++ User.java
@@ -0,0 +1,30 @@
+public class User implements Serializable {
+    protected String firstName;
+    protected String lastName;
+    protected String email;
+    protected String degreeProgram;
+
+    public User(String firstName, String lastName, String email, String degreeProgram) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.email = email;
+        this.degreeProgram = degreeProgram;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public String getDegreeProgram() {
+        return degreeProgram;
+    }
+
+}
